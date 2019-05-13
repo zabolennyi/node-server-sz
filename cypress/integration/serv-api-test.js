dca39ed2de8a34c09702e0c502bdbe1df40775a2
Serhii Zabolennyi <zabolennyy.sergey@gmail.com>
2019-05-13 17:42:16 +0200

    added cypress api tests and CI

---
+++ cypress/integration/serv-api-test.js
@@ -0,0 +1,27 @@
+const initialTodos = [
+    {
+        "success": "true",
+        "message": "todos retrieved successfully",
+        "todos": [
+            {
+                "id": 1,
+                "title": "coffee",
+                "description": "Go for coffee at 11am"
+            }
+        ]
+    }
+  ]
+
+  it('returns JSON', () => {
+    cy.request('api/todos')
+      .its('headers')
+      .its('content-type')
+      .should('include', 'application/json')
+  })
+
+  it('Validate the status', () => {
+     cy.request('api/todos')
+    .its('status')
+          .should('equal',200);
+  })
+
