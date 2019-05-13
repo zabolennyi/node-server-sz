c637c701ef5d0a353d9e0987910428424766f4b4
Serhii Zabolennyi <zabolennyy.sergey@gmail.com>
2019-05-13 12:34:06 +0200

    added post y get methods

---
+++ db/db.js
@@ -0,0 +1,9 @@
+const todos =  [
+    {
+      id: 1,
+      title: "coffee",
+      description: "Go for coffee at 11am"
+    }
+];
+
+export default todos;
--- server.js
+++ server.js
@@ -1,15 +1,45 @@
-// server.js
+import express from 'express';
+import db from './db/db';
+import bodyParser from 'body-parser';
 
-var express = require('express');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var app = express();
+app.get('/api/todos', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'todos retrieved successfully',
+    todos: db
+  })
+});
+const PORT = 5000;
 
-var PORT = 3000;
-
-app.get('/', function(req, res) {
-    res.status(200).send('Hi SZ');
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
 });
 
-app.listen(PORT, function() {
-    console.log('Server is running on PORT:',PORT);
-});
+app.post('/api/todos', (req, res) => {
+    if(!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is required'
+      });
+    } else if(!req.body.description) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'description is required'
+      });
+    }
+   const todo = {
+     id: db.length + 1,
+     title: req.body.title,
+     description: req.body.description
+   }
+   db.push(todo);
+   return res.status(201).send({
+     success: 'true',
+     message: 'todo added successfully',
+     todo
+   })
+  });
